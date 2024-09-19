1d4ba33fa446b35220eae177271956dea7f485db
shakil ahmed <shakilkhanhu@gmail.com>
2024-09-19 19:59:28 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:web_windows/edite_view.dart';
-import 'dart:async';
-
-import 'package:webview_windows/webview_windows.dart';
-import 'package:window_manager/window_manager.dart';
-import 'assets_view.dart'; // Import the new AssetsView
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+import 'assets_view.dart';
 
 final navigatorKey = GlobalKey<NavigatorState>();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await windowManager.ensureInitialized();
-
+  WebViewPlatform.instance = WebKitWebViewPlatform();
   runApp(const MyApp());
 }
 
@@ -26,10 +22,11 @@ class MyApp extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          navigatorKey: navigatorKey,
-          theme: ThemeData.dark(), // Set the app theme to dark
-          home: ExampleBrowser()),
+        debugShowCheckedModeBanner: false,
+        navigatorKey: navigatorKey,
+        theme: ThemeData.dark(),
+        home: ExampleBrowser(),
+      ),
     );
   }
 }
@@ -40,58 +37,16 @@ class ExampleBrowser extends StatefulWidget {
 }
 
 class _ExampleBrowser extends State<ExampleBrowser> {
-  final _controller = WebviewController();
-  final List<StreamSubscription> _subscriptions = [];
+  late final WebViewController _controller;
   bool _showAssets = false;
   bool _showEditPage = false;
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  Future<void> initPlatformState() async {
-    try {
-      await _controller.initialize();
-      _subscriptions
-          .add(_controller.containsFullScreenElementChanged.listen((flag) {
-        debugPrint('Contains fullscreen element: $flag');
-        windowManager.setFullScreen(flag);
-      }));
-
-      await _controller.setBackgroundColor(Colors.transparent);
-      await _controller.setPopupWindowPolicy(WebviewPopupWindowPolicy.deny);
-      await _controller.loadUrl('dfgdfg');
-      // https://text-to-3d--new1-7ebce.us-central1.hosted.app/
-
-      if (!mounted) return;
-      setState(() {});
-    } on PlatformException catch (e) {
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        showDialog(
-            context: context,
-            builder: (_) => AlertDialog(
-                  title: Text('Error'),
-                  content: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Code: ${e.code}'),
-                      Text('Message: ${e.message}'),
-                    ],
-                  ),
-                  actions: [
-                    TextButton(
-                      child: Text('Continue'),
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                    )
-                  ],
-                ));
-      });
-    }
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse('https://text-to-3d--new1-7ebce.us-central1.hosted.app/'));
   }
 
   Widget compositeView() {
@@ -99,58 +54,15 @@ class _ExampleBrowser extends State<ExampleBrowser> {
       return AssetsView();
     } else if (_showEditPage) {
       return EditPage();
-    }
-    if (!_controller.value.isInitialized) {
-      return const Text(
-        'Not Initialized',
-        style: TextStyle(
-          fontSize: 24.0,
-          fontWeight: FontWeight.w900,
-        ),
-      );
     } else {
-      return Column(
-        children: [
-          Expanded(
-              child: Stack(
-            children: [
-              Webview(
-                _controller,
-                permissionRequested: _onPermissionRequested,
-              ),
-              StreamBuilder<LoadingState>(
-                  stream: _controller.loadingState,
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData &&
-                        snapshot.data == LoadingState.loading) {
-                      return LinearProgressIndicator();
-                    } else {
-                      return SizedBox();
-                    }
-                  }),
-            ],
-          )),
-        ],
-      );
+      return WebViewWidget(controller: _controller);
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          // backgroundColor: Colors.grey[850],
-          // centerTitle: true,
-          // title: StreamBuilder<String>(
-          //   stream: _controller.title,
-          //   builder: (context, snapshot) {
-          //     return Text(
-          //       snapshot.hasData ? snapshot.data! : 'Text to 3D Converter',
-          //       style: const TextStyle(color: Colors.white),
-          //     );
-          //   },
-          // )
-          ),
+      appBar: AppBar(),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
@@ -173,9 +85,9 @@ class _ExampleBrowser extends State<ExampleBrowser> {
               onTap: () {
                 setState(() {
                   _showAssets = false;
+                  _showEditPage = false;
                 });
-                _controller.loadUrl(
-                    'https://text-to-3d--new1-7ebce.us-central1.hosted.app/');
+                _controller.loadRequest(Uri.parse('https://text-to-3d--new1-7ebce.us-central1.hosted.app/'));
                 Navigator.pop(context);
               },
             ),
@@ -185,6 +97,7 @@ class _ExampleBrowser extends State<ExampleBrowser> {
               onTap: () {
                 setState(() {
                   _showAssets = true;
+                  _showEditPage = false;
                 });
                 Navigator.pop(context);
               },
@@ -210,8 +123,7 @@ class _ExampleBrowser extends State<ExampleBrowser> {
                   builder: (BuildContext context) {
                     return AlertDialog(
                       title: Text('About'),
-                      content:
-                          Text('This is a Text to 3D Converter application.'),
+                      content: Text('This is a Text to 3D Converter application.'),
                       actions: <Widget>[
                         TextButton(
                           child: Text('Close'),
@@ -230,37 +142,5 @@ class _ExampleBrowser extends State<ExampleBrowser> {
       ),
       body: compositeView(),
     );
-  }
-
-  Future<WebviewPermissionDecision> _onPermissionRequested(
-      String url, WebviewPermissionKind kind, bool isUserInitiated) async {
-    final decision = await showDialog<WebviewPermissionDecision>(
-      context: navigatorKey.currentContext!,
-      builder: (BuildContext context) => AlertDialog(
-        title: const Text('WebView permission requested'),
-        content: Text('WebView has requested permission \'$kind\''),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () =>
-                Navigator.pop(context, WebviewPermissionDecision.deny),
-            child: const Text('Deny'),
-          ),
-          TextButton(
-            onPressed: () =>
-                Navigator.pop(context, WebviewPermissionDecision.allow),
-            child: const Text('Allow'),
-          ),
-        ],
-      ),
-    );
-
-    return decision ?? WebviewPermissionDecision.none;
-  }
-
-  @override
-  void dispose() {
-    _subscriptions.forEach((s) => s.cancel());
-    _controller.dispose();
-    super.dispose();
   }
 }
